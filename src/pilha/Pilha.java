9adab831586890c43468e842a1a456e660808725
Alexandre Defelicibus <adefelicibus@gmail.com>
2017-01-21 18:23:59 -0200

    Add more tests.

--- src/pilha/Pilha.java
+++ src/pilha/Pilha.java
@@ -2,11 +2,15 @@ package pilha;
 
 public class Pilha {
 	
-	private Object elemento;
-	private int quantidade;
+	private Object[] elementos;
+	private int quantidade = 0;
+
+	public Pilha(int i) {
+		elementos = new Object[i];
+	}
 
 	public boolean estaVazia() {
-		return (this.elemento == null);
+		return quantidade == 0;
 	}
 
 	public int tamanho() {	
@@ -14,12 +18,23 @@ public class Pilha {
 	}
 
 	public void empilha(Object elemento) {
-		this.elemento = elemento;
+		if(quantidade == elementos.length)
+			throw new PilhaCheiaException("Pilha cheia");
+
+		this.elementos[quantidade] = elemento;
 		this.quantidade++;
 	}
 
 	public Object topo() {
-		return this.elemento;
+		return this.elementos[quantidade - 1];
+	}
+
+	public Object desempilha() {
+		if(estaVazia())
+			throw new PilhaVaziaException("Pilha vazia");
+		Object topo = this.topo();
+		this.quantidade--;
+		return topo;
 	}
 
 }
--- src/pilha/TestePilha.java
+++ src/pilha/TestePilha.java
@@ -2,24 +2,70 @@ package pilha;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestePilha {
+	
+	private Pilha p;
+	
+	@Before
+	public void inicializaPilha(){
+		p = new Pilha(10);
+	}
 
 	@Test
 	public void pilhaVazia() {
-		Pilha p = new Pilha();
 		assertTrue(p.estaVazia());
 		assertEquals(0, p.tamanho());
 	}
 	
 	@Test
 	public void empilhaUmElemento() {
-		Pilha p = new Pilha();
 		p.empilha("primeiro");
 		assertFalse(p.estaVazia());
 		assertEquals(1, p.tamanho());
 		assertEquals("primeiro", p.topo());
+	}
+	
+	@Test
+	public void empilhaDoisElemento() {
+		p.empilha("primeiro");
+		p.empilha("segundo");
+		assertFalse(p.estaVazia());
+		assertEquals(2, p.tamanho());
+		assertEquals("segundo", p.topo());
+	}
+	
+	@Test
+	public void desempilha() {
+		p.empilha("primeiro");
+		p.empilha("segundo");
+		assertFalse(p.estaVazia());
+		assertEquals(2, p.tamanho());
+		assertEquals("segundo", p.topo());
+		
+		Object desempilhado = p.desempilha();
+		assertFalse(p.estaVazia());
+		assertEquals(1, p.tamanho());
+		assertEquals("primeiro", p.topo());
+		assertEquals("segundo", desempilhado);
+	}
+	
+	@Test(expected=PilhaVaziaException.class)
+	public void removeDaPilhaVazia(){
+		p.desempilha();
+	}
+	
+	@Test
+	public void adicionaNaPilhaCheia(){
+		for (int i = 0; i < 10; i++) {
+			p.empilha("elemento"+1);
+		}
+		try {
+			p.empilha("boom");
+			fail();
+		} catch (PilhaCheiaException e) {}
 	}
 
 }
